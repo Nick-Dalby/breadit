518d225416d0f661192fc76183935c2962c66f53
nick <nckdlby@gmail.com>
2022-11-12 19:24:23 +0100

    scafold feed and post components

---
+++ components/Post.tsx
@@ -0,0 +1,27 @@
+import { ArrowDownIcon, ArrowUpIcon, BookmarkIcon, ChatBubbleLeftIcon, EllipsisHorizontalIcon, GiftIcon, ShareIcon } from '@heroicons/react/24/outline'
+
+type Props = {
+  post: Post
+}
+
+const Post = ({ post }: Props) => {
+  return <div>
+    {/* upvotes */}
+  <div></div>
+
+  <div>
+    {/* header */}
+
+
+    {/* body */}
+
+
+    {/* image */}
+
+
+    {/* footer */}
+  </div>
+
+  </div>
+}
+export default Post
