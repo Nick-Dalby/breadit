1ecb08c467edf291774f71e8ac7d661e2afdd5c3
nick <nckdlby@gmail.com>
2022-11-12 19:23:00 +0100

    get all posts query

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,7 +1,7 @@
-import { gql } from '@apollo/client';
+import { gql } from '@apollo/client'
 
 export const GET_SUBBREAD_BY_FILLING = gql`
-  query MyQuery($filling: String!)   {
+  query MyQuery($filling: String!) {
     getSubbreadByFilling(filling: $filling) {
       created_at
       filling
@@ -10,5 +10,35 @@ export const GET_SUBBREAD_BY_FILLING = gql`
   }
 `
 
-
-
+export const GET_ALL_POSTS = gql`
+  query MyQuery {
+    getPostList {
+      body
+      created_at
+      id
+      image
+      title
+      subbread_id
+      username
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+      subbread {
+        created_at
+        filling
+        id
+      }
+    }
+  }
+`
