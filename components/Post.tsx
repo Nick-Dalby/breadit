3f4a4c3b76a408000ded782ee391c048984ed795
nick <nckdlby@gmail.com>
2022-11-14 19:30:41 +0100

    vote logic

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,3 +1,4 @@
+import { useMutation, useQuery } from '@apollo/client'
 import {
   ArrowDownIcon,
   ArrowUpIcon,
@@ -7,39 +8,114 @@ import {
   GiftIcon,
   ShareIcon,
 } from '@heroicons/react/24/outline'
-import Link from 'next/link'
-import TimeAgo from 'react-timeago'
-import Avatar from './Avatar'
 import { Orbit } from '@uiball/loaders'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-hot-toast'
+import TimeAgo from 'react-timeago'
+import { ADD_VOTE } from '../graphql/mutations'
+import { GET_ALL_VOTES_BT_POST_ID } from '../graphql/queries'
+import Avatar from './Avatar'
 
 type Props = {
   post: Post
 }
 
 const Post = ({ post }: Props) => {
-  
+  const { data: session } = useSession()
+  const [vote, setVote] = useState<boolean>()
+
+  const { data, loading } = useQuery(GET_ALL_VOTES_BT_POST_ID, {
+    variables: {
+      post_id: post?.id,
+    },
+  })
+
+  const [addVote] = useMutation(ADD_VOTE, {
+    refetchQueries: [GET_ALL_VOTES_BT_POST_ID, 'getVotesByPostId'],
+  })
+
+  useEffect(() => {
+    const votes: Vote[] = data?.getVotesByPostId
+
+    const vote = votes?.find(
+      (vote) => vote.username === session?.user?.name
+    )?.upvote
+
+    setVote(vote)
+  }, [data])
+
+  const upVote = async (isUpvote: boolean) => {
+    if (!session) {
+      toast('🚩 Sign in to vote!')
+      return
+    }
+
+    if (vote && isUpvote) return
+    if (vote === false && !isUpvote) return
+
+    console.log('voting: ', isUpvote)
+
+    await addVote({
+      variables: {
+        post_id: post.id,
+        username: session.user?.name,
+        upvote: isUpvote,
+      },
+    })
+  }
+
+  const displayVotes = (data: any) => {
+    const votes: Vote[] = data?.getVotesByPostId
+    const displayNumber = votes?.reduce((total, vote) => vote.upvote ? (total += 1) : (total -= 1), 0)
+
+    if (votes?.length === 0 ) return 0
+
+    if (displayNumber === 0) {
+      return votes[0]?.upvote ? 1 : -1
+    }
+
+    return displayNumber
+  }
+
   //adding a loading state, remove if implementing SSR?
-  if (!post) return (
-    <div className='flex w-full items-center justify-center p-10 text-xl'>
-      <Orbit size={50} color='#ff4520' />
-    </div>
-  )
+  if (!post)
+    return (
+      <div className="flex w-full items-center justify-center p-10 text-xl">
+        <Orbit size={50} color="#ff4520" />
+      </div>
+    )
 
   return (
     <Link href={`/post/${post.id}`} legacyBehavior>
       <div className=" flex cursor-pointer rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
         {/* upvotes */}
         <div className="flex flex-col items-center justify-start space-y-1 rounded-l-lg bg-white p-4 text-gray-400">
-          <ArrowUpIcon className="voteBtn hover:text-red-400" />
-          <p className="text-xs font-bold text-black">0</p>
-          <ArrowDownIcon className="voteBtn hover:text-blue-400" />
+          <ArrowUpIcon
+            onClick={(e) => {
+              e.stopPropagation()
+              upVote(true)
+            }}
+            className={`voteBtn hover:text-red-400 ${vote && 'text-red-400'}`}
+          />
+          <p className={`text-xs font-bold text-black`}>{displayVotes(data)}</p>
+          <ArrowDownIcon
+            onClick={(e) => {
+              e.stopPropagation()
+              upVote(false)
+            }}
+            className={`voteBtn hover:text-blue-400 ${
+              vote === false && 'text-blue-400'
+            }`}
+          />
         </div>
 
         <div className="p-3  pb-1 ">
           {/* header */}
           <div className="flex items-start space-x-2">
             <Avatar seed={post.subbread[0]?.filling} />
-            <p className="text-xs text-gray-400 lowercase">
+            <p className="text-xs lowercase text-gray-400">
               <Link href={`/subbread/${post.subbread[0]?.filling}`}>
                 <span className="font-bold text-black hover:text-blue-500 hover:underline">
                   br/{post.subbread[0]?.filling}
