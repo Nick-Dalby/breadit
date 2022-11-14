9cc0a13d3a4685da41b5ed7811e74cacee777ef3
nick <nckdlby@gmail.com>
2022-11-14 15:46:23 +0100

    links

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,42 +16,46 @@ import {
 } from '@heroicons/react/24/solid'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
   const { data: session } = useSession()
 
   return (
-    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm " >
-      <div className="flex flex-shrink-0 cursor-pointer items-center space-x-2">
-        <div className="relative h-10 w-10">
-          <Image
-            src="/breadface.svg"
-            alt="logo"
-            fill
-            className="object-contain"
-          />
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm items-center">
+      <Link href="/">
+        <div className="flex flex-shrink-0 cursor-pointer items-center space-x-2">
+          <div className="relative h-10 w-10">
+            <Image
+              src="/breadface.svg"
+              alt="logo"
+              fill
+              className="object-contain"
+            />
+          </div>
+          <div className="relative hidden h-10 w-20 sm:inline-block">
+            <Image
+              src="/breadit.svg"
+              alt="logo"
+              fill
+              className="object-contain"
+            />
+          </div>
         </div>
-        <div className="relative h-10 w-20 hidden sm:inline-block">
-          <Image
-            src="/breadit.svg"
-            alt="logo"
-            fill
-            className="object-contain"
-          />
+      </Link>
+      <Link href="/">
+        <div className="mx-7 flex items-center xl:min-w-[300px]">
+          <HomeIcon className="h-5 w-5" />
+          <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+          <ChevronDownIcon className="h-5 w-5" />
         </div>
-      </div>
-      <div className="mx-7 flex items-center xl:min-w-[300px]">
-        <HomeIcon className="h-5 w-5" />
-        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
-        <ChevronDownIcon className="h-5 w-5" />
-      </div>
-
-      <form className="hidden sm:flex flex-1 items-center space-x-2 rounded-full border border-gray-200 bg-gray-100 px-3 py-1">
+      </Link>
+      <form className="hidden flex-1 items-center space-x-2 rounded-full border border-gray-200 bg-gray-100 px-3 py-1 sm:flex">
         <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
         <input
           type="text"
           placeholder="Look for loaves"
-          className="flex-1 bg-transparent outline-none text-sm sm:text-base"
+          className="flex-1 bg-transparent text-sm outline-none sm:text-base"
         />
         <button type="submit" hidden></button>
       </form>
@@ -66,7 +70,7 @@ const Header = () => {
         <PlusIcon className="icon" />
         <MegaphoneIcon className="icon" />
       </div>
-      <div className="ml-auto sm:ml-5 flex items-center lg:hidden">
+      <div className="ml-auto flex items-center sm:ml-5 lg:hidden">
         <Bars3Icon className="icon" />
       </div>
 
@@ -84,12 +88,12 @@ const Header = () => {
               className="object-contain "
             />
           </div>
-          <div className='flex-1 text-xs'>
-          <p className='truncate'>{session?.user?.name}</p>
-          <p className="text-gray-400">20 Karma</p>
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">20 Karma</p>
           </div>
-        
-        <ChevronDoubleDownIcon className='h-5 flex-shrink-0 text-gray-400' />
+
+          <ChevronDoubleDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
       ) : (
         <div
--- components/Post.tsx
+++ components/Post.tsx
@@ -7,8 +7,9 @@ import {
   GiftIcon,
   ShareIcon,
 } from '@heroicons/react/24/outline'
-import Avatar from './Avatar'
+import Link from 'next/link'
 import TimeAgo from 'react-timeago'
+import Avatar from './Avatar'
 
 type Props = {
   post: Post
@@ -16,57 +17,66 @@ type Props = {
 
 const Post = ({ post }: Props) => {
   return (
-    <div className='flex cursor-pointer rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400'>
-      {/* upvotes */}
-      <div className='flex flex-col items-center justify-start space-y-1 rounded-l-lg bg-white p-4 text-gray-400'>
-        <ArrowUpIcon className='voteBtn hover:text-red-400' />
-        <p className='text-black font-bold text-xs'>0</p>
-        <ArrowDownIcon className='voteBtn hover:text-blue-400' />
+    <Link href={`/post/${post.id}`} legacyBehavior>
+      <div className=" flex cursor-pointer rounded-lg border border-gray-300 bg-white shadow-sm hover:border-gray-400">
+        {/* upvotes */}
+        <div className="flex flex-col items-center justify-start space-y-1 rounded-l-lg bg-white p-4 text-gray-400">
+          <ArrowUpIcon className="voteBtn hover:text-red-400" />
+          <p className="text-xs font-bold text-black">0</p>
+          <ArrowDownIcon className="voteBtn hover:text-blue-400" />
+        </div>
+
+        <div className="p-3  pb-1 ">
+          {/* header */}
+          <div className="flex items-start space-x-2">
+            <Avatar seed={post.subbread[0]?.filling} />
+            <p className="text-xs text-gray-400">
+              <Link href={`/subbread/${post.subbread[0]?.filling}`}>
+                <span className="font-bold text-black hover:text-blue-500 hover:underline">
+                  br/{post.subbread[0]?.filling}
+                </span>
+              </Link>
+              • Posted by u/{post.username} <TimeAgo date={post.created_at} />
+            </p>
+          </div>
+
+          {/* body */}
+          <div className="py-4 ">
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+            <p className="mt-2 text-sm font-light">{post.body}</p>
+          </div>
+
+          {/* image */}
+          <img src={post.image} alt="" className="w-full rounded-lg" />
+
+          {/* footer */}
+          <div className="mt-4 flex space-x-4 text-gray-400">
+            <div className="postBtns">
+              <ChatBubbleLeftIcon className="h-6 w-6" />
+              <p className="">
+                {post.comments.length}{' '}
+                {post.comments.length === 1 ? 'Comment' : 'Comments'}
+              </p>
+            </div>
+            <div className="postBtns">
+              <GiftIcon className="h-6 w-6" />
+              <p className="hidden sm:inline">Award</p>
+            </div>
+            <div className="postBtns">
+              <ShareIcon className="h-6 w-6" />
+              <p className="hidden sm:inline">Share</p>
+            </div>
+            <div className="postBtns">
+              <BookmarkIcon className="h-6 w-6" />
+              <p className="hidden sm:inline">Save</p>
+            </div>
+            <div className="postBtns">
+              <EllipsisHorizontalIcon className="h-6 w-6" />
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className='p-3  pb-1 '>
-        {/* header */}
-        <div className='flex items-start space-x-2'>
-          <Avatar seed={post.subbread[0]?.filling}/>
-          <p className='text-xs text-gray-400'>
-            <span className='font-bold text-black hover:text-blue-500 hover:underline'>br/{post.subbread[0]?.filling}</span> • Posted by u/{post.username} <TimeAgo date={post.created_at} />
-          </p>
-        </div>
-
-        {/* body */}
-        <div className='py-4 '>
-          <h2 className='text-xl font-semibold'>{post.title}</h2>
-          <p className='mt-2 text-sm font-light'>{post.body}</p>
-        </div>
-
-        {/* image */}
-        <img src={post.image} alt="" className='w-full rounded-lg'/>
-
-        {/* footer */}
-        <div className='flex space-x-4 text-gray-400 mt-4'>
-        <div className='postBtns'>
-          <ChatBubbleLeftIcon className='h-6 w-6'/>
-          <p className=''>{post.comments.length} {post.comments.length === 1 ? 'Comment' : 'Comments'}</p>
-        </div>
-        <div className='postBtns'>
-          <GiftIcon className='h-6 w-6'/>
-          <p className='hidden sm:inline'>Award</p>
-        </div>
-        <div className='postBtns'>
-          <ShareIcon className='h-6 w-6'/>
-          <p className='hidden sm:inline'>Share</p>
-        </div>
-        <div className='postBtns'>
-          <BookmarkIcon className='h-6 w-6'/>
-          <p className='hidden sm:inline'>Save</p>
-        </div>
-        <div className='postBtns'>
-          <EllipsisHorizontalIcon className='h-6 w-6'/>
-        </div>
-
-        </div>
-      </div>
-    </div>
+    </Link>
   )
 }
 export default Post
