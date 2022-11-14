cc6c0e1cde70566b607b9260e24cb1b509059d91
nick <nckdlby@gmail.com>
2022-11-14 15:45:51 +0100

    post by subbread queries

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -42,3 +42,36 @@ export const GET_ALL_POSTS = gql`
     }
   }
 `
+
+export const GET_ALL_POSTS_BY_FILLING = gql`
+  query MyQuery($filling: String!) {
+    getPostListByFilling(filling: $filling) {
+      body
+      created_at
+      id
+      image
+      title
+      subbread_id
+      username
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+      subbread {
+        created_at
+        filling
+        id
+      }
+    }
+  }
+`
